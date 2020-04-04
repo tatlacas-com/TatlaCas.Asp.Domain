9fdd604b4af6acc477bb9703c83f8d1c387e0261
Tatenda Caston Hove <tathove@tatlacas.com>
2020-04-04 22:15:38 +0200

    Add CellViewType to IResource

--- Resources/IResource.cs
+++ Resources/IResource.cs
@@ -6,6 +6,7 @@ namespace TatlaCas.Asp.Domain.Resources
     {
         public string ViewKey { get; set; }
         public string UniqueKey { get; set; }
+        public string CellViewType { get; set; }
         Dictionary<string, List<IResourceOption>> Options { get; set; }
         bool AddAnotherAfterSave { get; set; }
         bool ShowAddAnotherAfterSave { get; set; }
