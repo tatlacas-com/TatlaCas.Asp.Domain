9b5d5e0f91dee37afbd73cea39bfe500db75f8f3
Tatenda Caston Hove <tathove@tatlacas.com>
2020-04-04 13:12:33 +0200

    Add UniqueKey to IResource

--- Resources/IResource.cs
+++ Resources/IResource.cs
@@ -4,6 +4,7 @@ namespace TatlaCas.Asp.Domain.Resources
 {
     public interface IResource
     {
+        public string UniqueKey { get; set; }
         Dictionary<string, List<IResourceOption>> Options { get; set; }
         bool AddAnotherAfterSave { get; set; }
         bool ShowAddAnotherAfterSave { get; set; }
