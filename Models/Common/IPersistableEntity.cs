01125385a56eea6813549bd88b4d81297f94c11a
Tatenda <tathove@gmail.com>
2021-10-03 12:28:03 +0200

    add CreatedAt and UpdatedAt to entities

--- Models/Common/IPersistableEntity.cs
+++ Models/Common/IPersistableEntity.cs
@@ -1,9 +1,12 @@
+using System;
 using Microsoft.EntityFrameworkCore;
 
 namespace TatlaCas.Asp.Domain.Models.Common
 {
     public interface IPersistableEntity
     {
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
         void OnModelCreating(ModelBuilder builder);
     }
 }
--- Models/Common/PersistableEntity.cs
+++ Models/Common/PersistableEntity.cs
@@ -19,6 +19,8 @@ namespace TatlaCas.Asp.Domain.Models.Common
 
     public abstract class PersistableEntity<T> : BaseEntity, IPersistableEntity where T : class, IEntity
     {
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
         public string SearchProp => null;
         public void OnModelCreating(ModelBuilder builder)
         {
